90b8669075f2f9549cfb79b428ba64db62ca27da
josephmendieta <jyma1204@gmail.com>
2024-04-29 17:59:57 -0500

    Primer commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { AnimalComponent } from './components/animal/animal.component';
+
+export const routes: Routes = [
+    {path:'animal', component:AnimalComponent}
+];
